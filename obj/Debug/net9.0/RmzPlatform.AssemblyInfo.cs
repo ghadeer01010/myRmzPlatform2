c8e5a83fd71aef12328b3735a08cc6d7b3b7b4f9
ghadeer01010 <ghadeer2001@gmail.com>
2025-07-31 19:38:32 +0300

    Upload by ghadeer01010

--- obj/Debug/net9.0/RmzPlatform.AssemblyInfo.cs
+++ obj/Debug/net9.0/RmzPlatform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RmzPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aead0f200bd5d93cdb8468c6328683846438018")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e8e12d849d9a06e90e193c5fa61df239bab90c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RmzPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RmzPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
